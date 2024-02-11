7e33b38d61a1d5ff7e5a12698dd4734e3260ac5f
Kang Pyo <pyokang26@gmail.com>
2024-02-11 08:54:04 +0700

    nit: format with eslint

--- lib/utils.ts
+++ lib/utils.ts
@@ -39,8 +39,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateRandomName(): string {
-  const randomAdjective =
-    adjectives[Math.floor(Math.random() * adjectives.length)];
+  const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
   const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
 
   return `${randomAdjective} ${randomAnimal}`;
