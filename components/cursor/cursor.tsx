7857b6a2e81866be543a4f4b1df01688a0c1229b
Kang Pyo <pyokang26@gmail.com>
2024-02-13 09:48:28 +0700

    feature(cursor-chat): Display other user's cursor message on client

--- components/cursor/cursor.tsx
+++ components/cursor/cursor.tsx
@@ -13,8 +13,14 @@ export const Cursor = ({ color, x, y, message }: TProps) => {
       className="pointer-events-none absolute left-0 top-0"
       style={{ transform: `translate(${x}px, ${y}px)` }}
     >
+      {/* Cursor */}
       <CursorSVG color={color} />
-      {/* TODO: Implement message bar */}
+      {/* Cursor message */}
+      {message && (
+        <div className="absolute left-2 top-5 rounded-3xl px-4 py-2" style={{ backgroundColor: color }}>
+          <p className="text-white whitespace-nowrap text-sm leading-relaxed">{message}</p>
+        </div>
+      )}
     </div>
   );
 };
